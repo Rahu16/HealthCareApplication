df19fd9d9eb5a0f8d38a4ef40422e63f269084a5
Rahu16 <hasibul.cpc@gmail.com>
2021-06-16 15:34:56 +0530

    Renamed the repository

--- src/main/java/com/health/boot/repository/AppointmentRepository.java
+++ src/main/java/com/health/boot/repository/AppointmentRepository.java
@@ -1,4 +1,4 @@
-package com.health.boot.repo;
+package com.health.boot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
