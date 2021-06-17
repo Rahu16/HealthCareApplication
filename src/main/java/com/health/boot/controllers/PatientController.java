25bf11f227864dea7344bfaedaa3f70e6f47a518
ManikantaSatishPalacharla <85877892+ManikantaSatishPalacharla@users.noreply.github.com>
2021-06-17 15:00:05 +0530

    cancel appointment

--- src/main/java/com/health/boot/controllers/PatientController.java
+++ src/main/java/com/health/boot/controllers/PatientController.java
@@ -6,9 +6,11 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +54,7 @@ public class PatientController {
 	}
 
 	@GetMapping("{patientName}/{appointmentId}")
-	public String showAppointmentStatus(@PathVariable("patientId") String patientName,@PathVariable("appointmentId") int appointmentId) {
+	public String showAppointmentStatus(@PathVariable("patientName") String patientName,@PathVariable("appointmentId") int appointmentId) {
 		Patient p=ps.viewPatient(patientName);
 		Set<Appointment> set= as.viewAppointments(patientName);
 		for(Appointment a:set) {
@@ -60,6 +62,25 @@ public class PatientController {
 				return "Status is: "+a.getApprovalStatus();
 			}
 		}
-		return "No Appointment Found with id: "+appointmentId +" with Patient Name "+patientName;
+		return "No Appointment Found with id: "+appointmentId +" under Patient Name "+patientName;
+	}
+	
+	@DeleteMapping("{patientName}/{appointmentId}")
+	public String cancelAppointment(@PathVariable("patientName") String patientName,@PathVariable("appointmentId") int appointmentId) {
+		Patient p=ps.viewPatient(patientName);
+		Set<Appointment> set= as.viewAppointments(patientName);
+		for(Appointment a:set) {
+			if(a.getId()==appointmentId) {
+				if(a.getApprovalStatus().equals("approved")) {
+					return "Can't Cancel Appointemnet Once It Is Approved";
+				}
+				else {
+					Appointment apt=as.viewAppointment(appointmentId);
+					as.removeAppointment(apt);
+					return "appointment deleted";
+				}
+			}
+		}
+		return "No Appointment Found with id: "+appointmentId +" under Patient Name "+patientName;
 	}
 }
