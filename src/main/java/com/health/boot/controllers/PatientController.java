23321d5790a99216e2c1be191e4c7cffca5b2149
Venkatesh E <venkatesh.e.281@gmail.com>
2021-06-18 14:42:33 +0530

    Edited

--- src/main/java/com/health/boot/controllers/PatientController.java
+++ src/main/java/com/health/boot/controllers/PatientController.java
@@ -34,7 +34,8 @@ import com.health.boot.services.ITestResultServiceImpl;
 
 @RestController
 @RequestMapping("patient")
-public class PatientController {
+public class PatientController 
+{
 	
 
 	@Autowired
@@ -54,14 +55,16 @@ public class PatientController {
 	
 
 	@PostMapping("/registerPatient")
-	public ResponseEntity<String> registerPatient(@RequestBody Patient p) {
+	public ResponseEntity<String> registerPatient(@RequestBody Patient p) 
+	{
 		ps.registerPatient(p);
 		return new ResponseEntity<String>("Resgistered Sucessfully",HttpStatus.ACCEPTED);
 	}
 	
 
 	@PostMapping("/requestAppointment")
-	public ResponseEntity<Appointment> requestAppoinment(@RequestBody ObjHolderRequestAppointment requestAppoint) {
+	public ResponseEntity<Appointment> requestAppoinment(@RequestBody ObjHolderRequestAppointment requestAppoint) 
+	{
 		System.out.println("Hello Everyone");
 		Set<DiagnosticTest> set = new HashSet<>();
 		Patient p = ps.viewPatient(requestAppoint.getPatientName());
@@ -79,50 +82,65 @@ public class PatientController {
 		return new ResponseEntity<Appointment>(as.addAppointment(a),HttpStatus.CREATED);
 	}
 
+	
 	@GetMapping("/{patientName}/{appointmentId}/showstatus")
-	public ResponseEntity<String> showAppointmentStatus(@PathVariable("patientName") String patientName,@PathVariable("appointmentId") int appointmentId) {
+	public ResponseEntity<String> showAppointmentStatus(@PathVariable("patientName") String patientName,@PathVariable("appointmentId") int appointmentId) 
+	{
 		ps.viewPatient(patientName);
 		Set<Appointment> set= as.viewAppointments(patientName);
-		for(Appointment a:set) {
-			if(a.getId()==appointmentId) {
+		for(Appointment a:set) 
+		{
+			if(a.getId()==appointmentId) 
+			{
 				return new ResponseEntity<String>("Status is: "+a.getApprovalStatus(),HttpStatus.FOUND);
 			}
 		}
 		throw new AppointmentNotFoundException("Appointment is Not Found to See the Status");
 	}
 	
+	
 	@DeleteMapping("/{patientName}/{appointmentId}")
 	public ResponseEntity<String> cancelAppointment(@PathVariable("patientName") String patientName,@PathVariable("appointmentId") int appointmentId) {
 		ps.viewPatient(patientName);
 		Set<Appointment> set= as.viewAppointments(patientName);
-		for(Appointment a:set) {
-			if(a.getId()==appointmentId) {
-				if(a.getApprovalStatus().equals("approved")) {
+		for(Appointment a:set) 
+		{
+			if(a.getId()==appointmentId) 
+			{
+				if(a.getApprovalStatus().equals("approved")) 
+				{
 					return new ResponseEntity<String>("Can't Cancel Appointemnet Once It Is Approved",HttpStatus.FORBIDDEN);
 				}
-				else {
+				else 
+				{
 					Appointment apt=as.viewAppointment(appointmentId);
 					as.removeAppointment(apt);
 					return new ResponseEntity<String>("appointment deleted",HttpStatus.ACCEPTED);
 				}
 			}
 		}
+		
 		throw new AppointmentNotFoundException("Appointment is Not Found to Delete");
+	
 	}
 	
+	
 	@GetMapping("/{patientId}/alltestresults")
-	public ResponseEntity<Set> getAllTestResult(@PathVariable("patientId") int id){
+	public ResponseEntity<Set> getAllTestResult(@PathVariable("patientId") int id)
+	{
 		return new ResponseEntity<Set>(trs.viewResultsByPatient(ps.viewPatient(id)),HttpStatus.OK);
 	}
 	
+	
 	@GetMapping("/{patientId}/{appointId}/alltestresults")
-	public ResponseEntity<?> getAllTestResultByAppointment(@PathVariable("patientId") int patientId, @PathVariable("appointId") int appointId){
+	public ResponseEntity<?> getAllTestResultByAppointment(@PathVariable("patientId") int patientId, @PathVariable("appointId") int appointId)
+	{
 		ps.viewPatient(patientId);
 		Appointment a = as.viewAppointment(appointId);
 		if(a.getPatient().getPatientId()==patientId)
 			return new ResponseEntity<Set>(a.getTestResult(),HttpStatus.FOUND);
+		
 		throw new AppointmentNotFoundException("Appointment Not Found For the Particular Patient");
-
 		
 	}
 }
