3ee4d951a2b3e5944ec3d329379171d58c52ced5
Venkatesh E <venkatesh.e.281@gmail.com>
2021-06-18 13:49:38 +0530

    Edited

--- src/main/java/com/health/boot/controllers/AdminController.java
+++ src/main/java/com/health/boot/controllers/AdminController.java
@@ -58,96 +58,122 @@ public class AdminController {
 	
 	
 	@PostMapping("/newDiagnosticCenter")
-	public ResponseEntity<DiagnosticCenter> createNewDiagnosticCenter(@RequestBody DiagnosticCenter dc) {
-		
+	public ResponseEntity<DiagnosticCenter> createNewDiagnosticCenter(@RequestBody DiagnosticCenter dc)
+	{
 		return new ResponseEntity<DiagnosticCenter>(dcs.createDiagnosticCenter(dc),HttpStatus.CREATED);
 	}
 	
+	
 	@PostMapping("/createNewTest")
-	public ResponseEntity<DiagnosticTest> createNewTest(@RequestBody DiagnosticTest t) {
+	public ResponseEntity<DiagnosticTest> createNewTest(@RequestBody DiagnosticTest t) 
+	{
 		return new ResponseEntity<DiagnosticTest>(dts.CreateNewTest(t),HttpStatus.CREATED);
 	}
-		
+	
+	
 	@PutMapping("/updateTestDetails")
-	public ResponseEntity<DiagnosticTest> updateTestDetails(@RequestBody DiagnosticTest test) {
+	public ResponseEntity<DiagnosticTest> updateTestDetails(@RequestBody DiagnosticTest test)
+	{
 		return new ResponseEntity<DiagnosticTest>(dts.updateTestDetail(test),HttpStatus.ACCEPTED);
 	}
+	
 	
 	@PutMapping("/updateCenterDetails")
 	public ResponseEntity<String> updateCenterDetails(@RequestBody DiagnosticCenter dc) {
 		return new ResponseEntity<String>(dcs.updateDiagnosticCenter(dc),HttpStatus.ACCEPTED);
 	}
 	
+	
 	@PutMapping("/addTestInCenter/{tid}/{cid}")
 	public ResponseEntity<String> addTestInCenter(@PathVariable("cid") int cid,@PathVariable("tid") int tid) {
 		return new ResponseEntity<String>(dcs.addTestInCenter(tid, cid),HttpStatus.ACCEPTED);
 	}
 	
+	
 	@PutMapping("/delTestFromCenter/{cid}/{tid}")
-	public ResponseEntity<DiagnosticTest> delTestFromCenter(@PathVariable("cid") int cid,@PathVariable("tid") int tid) {
+	public ResponseEntity<DiagnosticTest> delTestFromCenter(@PathVariable("cid") int cid,@PathVariable("tid") int tid)
+	{
 		return new ResponseEntity<DiagnosticTest>(dts.removeTestFromDiagnosticCenter(cid, tid),HttpStatus.ACCEPTED);
 	}
 	
+	
 	@DeleteMapping("/deleteDiagnosticCenter/{id}")
-	public ResponseEntity<String> deleteDiagnosticCenter(@PathVariable("id") int id) {
+	public ResponseEntity<String> deleteDiagnosticCenter(@PathVariable("id") int id) 
+	{
 		return new ResponseEntity<String>(dcs.removeDiagnosticCenter(id),HttpStatus.ACCEPTED);
 	}
 	
+	
 	@PutMapping("/changeAppointmentStatus/{appointId}/{status}")
-	public ResponseEntity<String> changeStatus(@PathVariable("appointId") int appointId, @PathVariable("status") ApprovalStatus status) {
+	public ResponseEntity<String> changeStatus(@PathVariable("appointId") int appointId, @PathVariable("status") ApprovalStatus status)
+	{
 		Appointment a = asi.viewAppointment(appointId);
 		a.setApprovalStatus(status);
 		asi.updateAppointment(a);
 		return new ResponseEntity<String>("Status is Changed",HttpStatus.ACCEPTED);
 	}
 	
+	
 	@GetMapping("/getalltestResults")
-	public ResponseEntity<List> getAllTestResults(){
+	public ResponseEntity<List> getAllTestResults()
+	{
 		return new ResponseEntity<List>(trs.getAllTestResults(),HttpStatus.FOUND);
 	}
 	
+	
 	@PostMapping("/{appointId}/addTestResult")
-	public ResponseEntity<String> addTestToAppointment(@PathVariable("appointId") int appointId, @RequestBody TestResult tr) {
+	public ResponseEntity<String> addTestToAppointment(@PathVariable("appointId") int appointId, @RequestBody TestResult tr)
+	{
 		Appointment a = asi.viewAppointment(appointId);
 		a.getTestResult().add(tr);
 		asi.updateAppointment(a);
 		return new ResponseEntity<String>("Test result is added to the Appointment",HttpStatus.ACCEPTED);
 	}
 	
+	
 	@GetMapping("/getAppointmentList/{status}")
-	public ResponseEntity<List> getAppointmentsByStatus(@PathVariable("status") ApprovalStatus status){
+	public ResponseEntity<List> getAppointmentsByStatus(@PathVariable("status") ApprovalStatus status)
+	{
 		List<Appointment> appointmentList = ar.findAll();
 		return new ResponseEntity<List>(appointmentList.stream().filter(a->a.getApprovalStatus().equals(status)).collect(Collectors.toList()),HttpStatus.FOUND);
 	}
 	
+	
 	@GetMapping("/getAllPatient")
-	public ResponseEntity<List> getAllPatient(){
+	public ResponseEntity<List> getAllPatient()
+	{
 		return new ResponseEntity<List>(pr.findAll(),HttpStatus.OK);
 	}
 	
+	
 	@DeleteMapping("/removePatient/{patientId}")
-	public ResponseEntity<String> deletePatient(@PathVariable("patientId") int id){
+	public ResponseEntity<String> deletePatient(@PathVariable("patientId") int id)
+	{
 		psi.removePatient(id);
 		return new ResponseEntity<String>("Patient is Deleted",HttpStatus.ACCEPTED);
 	
 	}
 	
+	
 	@DeleteMapping("/removeAppointment/{appointId}")
-	public ResponseEntity<String> deleteAppointment(@PathVariable("appointId") int id){
+	public ResponseEntity<String> deleteAppointment(@PathVariable("appointId") int id)
+	{
 		asi.removeAppointment(asi.viewAppointment(id));
 		return new ResponseEntity<String>("Appointment is Deleted",HttpStatus.ACCEPTED);
-	
 	}
+	
 	
 	@DeleteMapping("/removeTestResult/{testId}")
-	public ResponseEntity<String> delete(@PathVariable("testId") int id){
+	public ResponseEntity<String> delete(@PathVariable("testId") int id)
+	{
 		trs.removeTestResult(id);
 		return new ResponseEntity<String>("Test Result is Deleted",HttpStatus.ACCEPTED);
-	
 	}
 	
+	
 	@GetMapping("/show/{id}")
-	public ResponseEntity<Patient> seePatient(@PathVariable("id") int id){
+	public ResponseEntity<Patient> seePatient(@PathVariable("id") int id)
+	{
 		return new ResponseEntity<Patient>(psi.viewPatient(id),HttpStatus.ACCEPTED);
 	}
 }
