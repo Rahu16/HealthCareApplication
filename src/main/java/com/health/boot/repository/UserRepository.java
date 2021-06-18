ed2c322746cef88347ab1f993332b34cca63a8ec
Venkatesh E <venkatesh.e.281@gmail.com>
2021-06-18 16:41:32 +0530

    Edited

--- src/main/java/com/health/boot/repository/UserRepository.java
+++ src/main/java/com/health/boot/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.health.boot.entities.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, String>
+{
 
-	
 }
