325f13406e54279cddfe1dbc0968dd521a1f4b15
Venkatesh E <venkatesh.e.281@gmail.com>
2021-06-18 15:31:49 +0530

    Edited

--- src/main/java/com/health/boot/services/IAdminServiceImpl.java
+++ src/main/java/com/health/boot/services/IAdminServiceImpl.java
@@ -10,7 +10,8 @@ import com.health.boot.exceptions.UserNotFoundException;
 import com.health.boot.repository.UserRepository;
 
 @Service
-public class IAdminServiceImpl implements IAdminService{
+public class IAdminServiceImpl implements IAdminService
+{
 
 	@Autowired
 	IUserServiceImpl usi;
@@ -19,14 +20,16 @@ public class IAdminServiceImpl implements IAdminService{
 	UserRepository ur;
 	
 	@Override
-	public void registerAdmin(String username, String password) throws RuntimeException {
+	public void registerAdmin(String username, String password) throws RuntimeException 
+	{
 		Optional<User> u = ur.findById(username);
-		if(u.get().getRole()=="Admin") {
+		if(u.get().getRole()=="Admin") 
+		{
 			usi.addUser(new User(10,username,password,"Admin"));
 			return;
 		}
+		
 		throw new UserNotFoundException("Admin is Not Found with this User Name: "+username);
 		
 	}
-
 }
