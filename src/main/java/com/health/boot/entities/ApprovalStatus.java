c9a9bd74868208b6a5bac177538ae1a41a38b932
Venkatesh E <venkatesh.e.281@gmail.com>
2021-06-18 16:09:01 +0530

    Edited

--- src/main/java/com/health/boot/entities/ApprovalStatus.java
+++ src/main/java/com/health/boot/entities/ApprovalStatus.java
@@ -1,20 +1,24 @@
 package com.health.boot.entities;
 
-public enum ApprovalStatus {
+public enum ApprovalStatus 
+{
 
 	approved("approved"),
 	pending("pending"),
 	cancelled("cancelled");
 	
-	ApprovalStatus(String status ) {
+	ApprovalStatus(String status ) 
+	{
 		this.setStatus(status);
 	}
-	public String getStatus() {
+	
+	public String getStatus() 
+	{
 		return status;
 	}
-	public void setStatus(String status) {
+	public void setStatus(String status) 
+	{
 		this.status = status;
 	}
 	private String status;
-	
-}
+	}
