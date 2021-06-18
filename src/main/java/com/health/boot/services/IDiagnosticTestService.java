9470c0dd862465dc95f09884bc341e521991167f
Venkatesh E <venkatesh.e.281@gmail.com>
2021-06-18 15:51:18 +0530

    Edited

--- src/main/java/com/health/boot/services/IDiagnosticTestService.java
+++ src/main/java/com/health/boot/services/IDiagnosticTestService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import com.health.boot.entities.DiagnosticTest;
 
-public interface IDiagnosticTestService {
+public interface IDiagnosticTestService 
+{
+	
 	public List<DiagnosticTest> getAllTest();
 	DiagnosticTest CreateNewTest(DiagnosticTest test);
  	List<DiagnosticTest> getTestsOfDiagnosticCenter(int centerId);
