85e59ac4d0d277f20ab2853b286d3d15b94c4783
Venkatesh E <venkatesh.e.281@gmail.com>
2021-06-18 14:59:41 +0530

    Edited

--- src/main/java/com/health/boot/exceptions/AllreadyDiagnosticTestExistException.java
+++ src/main/java/com/health/boot/exceptions/AllreadyDiagnosticTestExistException.java
@@ -2,6 +2,7 @@ package com.health.boot.exceptions;
 
 public class AllreadyDiagnosticTestExistException extends RuntimeException
 {
+	
 	public AllreadyDiagnosticTestExistException(String msg) 
 	{
 		super(msg);
